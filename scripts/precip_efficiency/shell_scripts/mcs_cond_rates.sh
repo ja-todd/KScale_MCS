6d7aea09db0969a50e7e50a82e588cc5317dd59e
JT <jammietodd7@gmail.com>
2026-08-04 19:53:12 +0100

    removed the z10 models from the script because they're not currently used

--- scripts/precip_efficiency/shell_scripts/mcs_cond_rates.sh
+++ scripts/precip_efficiency/shell_scripts/mcs_cond_rates.sh
@@ -3,7 +3,7 @@
 source ~/miniforge3/bin/activate
 conda activate hk26_env
 
-MODELS=(um_glm_n2560_RAL3p3_tuned_hk26 um_glm_n2560_RAL3p3_tuned_sahel_z10_t40k um_glm_n2560_RAL3p3_tuned_sahel_z10_t4k um_glm_n2560_CoMA9_hk26 um_glm_n1280_GAL9_v2_hk26 um_glm_n1280_CoMA9_hk26)
+MODELS=(um_glm_n2560_RAL3p3_tuned_hk26 um_glm_n2560_CoMA9_hk26 um_glm_n1280_GAL9_v2_hk26 um_glm_n1280_CoMA9_hk26)
 
 for model in "${MODELS[@]}"; do 
 	time python ../mcs_condensation_rates.py --model "$model"
--- scripts/precip_efficiency/shell_scripts/run_PE_lifecycle_scripts.sh
+++ scripts/precip_efficiency/shell_scripts/run_PE_lifecycle_scripts.sh
@@ -3,8 +3,8 @@
 source ~/miniforge3/bin/activate
 conda activate hk26_env
 
-MODELS=(um_glm_n2560_RAL3p3_tuned_hk26 um_glm_n2560_RAL3p3_tuned_sahel_z10_t40k um_glm_n2560_RAL3p3_tuned_sahel_z10_t4k um_glm_n2560_CoMA9_hk26 um_glm_n1280_GAL9_v2_hk26 um_glm_n1280_CoMA9_hk26)
+MODELS=(um_glm_n2560_RAL3p3_tuned_hk26 um_glm_n2560_CoMA9_hk26 um_glm_n1280_GAL9_v2_hk26 um_glm_n1280_CoMA9_hk26)
 
 for model in "${MODELS[@]}"; do 
-	time python ../test_1h_norm_lifecycle.py --model "$model"
+	time python ../build_1h_norm_lifecycle_dataset.py --model "$model"
 done 
