ff77e21a1cd32f1d1459b6d4f30e12a9e2e69f8e
JT <jammietodd7@gmail.com>
2026-08-06 18:34:21 +0100

    fixed the lifecycle plots so the lines actually reach 100%, and the percentage contribution totals 100% as well.

--- scripts/precip_efficiency/PE_plots.py
+++ scripts/precip_efficiency/PE_plots.py
@@ -1,15 +1,13 @@
+from pathlib import Path
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt 
+import numpy as np
 import src.hp_models as models 
 import src.hp_utils as utils 
-from collections import defaultdict
-import matplotlib.pyplot as plt 
 import src.plot_utils as p_utils 
-import xarray as xr
-import numpy as np
-import time 
-from pathlib import Path
-import pickle 
-from scipy.interpolate import interp1d
 import warnings
+import xarray as xr
+
 
 
 with warnings.catch_warnings():
@@ -24,8 +22,8 @@ MODEL_NAMES, REGION_CFG, COLORS = p_utils.plot_var_setup()
 
 p_utils.apply_plot_style()
 
-
-tracks_list = utils.get_tracks_list()
+cache_path = Path('../../data/tracks_list_cache.pkl')
+tracks_list = utils.get_tracks_list(MODEL_NAMES, REGION_CFG, cache_path)
 
     
 
@@ -41,14 +39,14 @@ def _plot_lifecycle_mean_cr(n_bins=20):
         
         dstracks = model_tracks
 
-        PE_zarr = xr.open_zarr(f'/gws/ssde/j25b/mcs_prime/jtodd/precip_efficiency/data/{model_pid}/mcs_condensation_rate_wam_stats.zarr')
+        PE_zarr = xr.open_zarr(f'{BASE_PATH}{model_pid}/mcs_condensation_rate_wam_stats.zarr')
 
 
         dstracks.track_duration.load()
         durations_hours = dstracks.track_duration.values  # (n_tracks,) in hours
 
-        cr_values = PE_zarr.cr_mean.values 
-        pr_values = PE_zarr.pr_mean.values # (tracks, times_3h)
+        cr_values = PE_zarr.cr_sum.values 
+        pr_values = PE_zarr.pr_sum.values # (tracks, times_3h)
         n_tracks, n_times = cr_values.shape  # (tracks, times_3h)
 
 
@@ -61,20 +59,21 @@ def _plot_lifecycle_mean_cr(n_bins=20):
         lifecycle_positions = np.linspace(0, 1, n_bins)
 
         for tr in range(n_tracks):
-            n_valid = (~np.isnan(cr_values[tr])).sum()
-            
+            valid_mask = ~np.isnan(cr_values[tr])
+            valid_cr = cr_values[tr, valid_mask]
+            valid_pr = pr_values[tr, valid_mask]
+            n_valid = valid_mask.sum()
+
             if n_valid < 2:
                 continue
-            
-            valid_cr = cr_values[tr, :n_valid]
-            valid_pr = pr_values[tr, :n_valid]
 
             dur_hours = durations_hours[tr]
-            frac_positions = (np.arange(n_valid) * 3) / dur_hours
+            valid_indices = np.where(valid_mask)[0]
+            frac_positions = (valid_indices * 3) / dur_hours
             frac_positions = np.clip(frac_positions, 0, 1)
 
-            f_cr = interp1d(frac_positions, valid_cr, bounds_error=False, fill_value=np.nan)
-            f_pr = interp1d(frac_positions, valid_pr, bounds_error=False, fill_value=np.nan)
+            f_cr = interp1d(frac_positions, valid_cr, bounds_error=False, fill_value=(np.nan, valid_cr[-1]))
+            f_pr = interp1d(frac_positions, valid_pr, bounds_error=False, fill_value=(np.nan, valid_pr[-1]))
 
             binned_cr[tr] = f_cr(lifecycle_positions)
             binned_pr[tr] = f_pr(lifecycle_positions)
@@ -84,23 +83,26 @@ def _plot_lifecycle_mean_cr(n_bins=20):
         lifecycle_pctg    = np.linspace(0, 100, n_bins)
 
         ax.plot(lifecycle_pctg, lifecycle_mean_cr, color=color, label=mname)
-        ax1.plot(lifecycle_pctg, lifecycle_mean_pr, color=color, label=mname, linestyle='--')
+        ax1.plot(lifecycle_pctg, lifecycle_mean_pr, color=color, linestyle='--')
 
-    ax.plot([], [], color='grey', alpha=0.5, linestyle= '--', label='precip flux')
-    ax.plot([], [], color='grey', alpha=0.5, linestyle= '-', label='condensation rate')
-    ax.legend(bbox_to_anchor = (1.2, 1.3), ncols=3)
-    ax.set_xlabel(r"$\%$ of MCS lifecyle")
-    ax.set_ylabel(r"Condensation rate [kg m$^2$ s$^{-1}$]")
-    ax1.set_ylabel(r"Precipitation flux [kg m$^2$ s$^{-1}$]")
+    ax.plot([], [], color='grey', alpha=0.5, linestyle= '--', label='precipitation')
+    ax.plot([], [], color='grey', alpha=0.5, linestyle= '-', label='condensation')
+    # ax.legend(bbox_to_anchor = (1.2, 1.3), ncols=3)
+    ax.set_xlabel(r"$\%$ MCS lifecyle")
+    ax.set_ylabel(r"Condensation rate [kg m$^{-2}$ s$^{-1}$]")
+    ax1.set_ylabel(r"Precipitation rate [kg m$^{-2}$ s$^{-1}$]")
     # ax.set_ylim(0.2, 1.)
     ax.spines['right'].set_visible(True)
     ax.grid(color='white')
-    plt.savefig('figs/cr_pr_lifecycle.png', bbox_inches='tight')
+    ax1.grid(False)
+    p_utils.centre_legend_above(fig, ax, ncols=3)
+    plt.savefig('figs/lifecycles/crSUM_prSUM_lifecycle')
     plt.close()
 
 
-def _plot_cr_lifecycle_contribution(n_bins=20): 
+def _plot_cr_pr_lifecycle_contribution(n_bins=20): 
     fig, ax = plt.subplots()
+    ax1 = ax.twinx()
     # ax1 = ax.twinx()
 
     for mname, color, model_tracks in zip(MODEL_NAMES, COLORS, tracks_list): 
@@ -109,55 +111,78 @@ def _plot_cr_lifecycle_contribution(n_bins=20):
             
         dstracks = model_tracks
 
-        PE_zarr = xr.open_zarr(f'/gws/ssde/j25b/mcs_prime/jtodd/precip_efficiency/data/{model_pid}/mcs_condensation_rate_wam_stats.zarr')
+        PE_zarr = xr.open_zarr(f'{BASE_PATH}{model_pid}/mcs_condensation_rate_wam_stats.zarr')
         
         
 
         dstracks.track_duration.load()
         durations_hours = dstracks.track_duration.values  # (n_tracks,) in hours
 
-        cr_values = PE_zarr.cr_mean.values 
-        pr_values = PE_zarr.pr_mean.values # (tracks, times_3h)
+        cr_values = PE_zarr.cr_sum.values 
+        pr_values = PE_zarr.pr_sum.values # (tracks, times_3h)
         n_tracks, n_times = cr_values.shape  # (tracks, times_3h)
 
 
         
 
-        bin_values = np.full((n_tracks, n_bins), np.nan)
+        cr_bin_values = np.full((n_tracks, n_bins), np.nan)
+        pr_bin_values = np.full((n_tracks, n_bins), np.nan)
         lifecycle_positions = np.linspace(0, 1, n_bins)
 
         for tr in range(n_tracks):
-            n_valid = (~np.isnan(cr_values[tr])).sum()
+            valid_mask = ~np.isnan(cr_values[tr])
+            valid_cr = cr_values[tr, valid_mask]
+            valid_pr = pr_values[tr, valid_mask]
+            n_valid = valid_mask.sum()
+
             if n_valid < 2:
                 continue
-            
-            valid_cr = cr_values[tr, :n_valid]
-            total_cr = valid_cr.sum()
-            pctg_contr = (valid_cr / total_cr) * 100
-            
-            dur_hours = durations_hours[tr]
-            frac_positions = (np.arange(n_valid) * 3) / dur_hours
-            frac_positions = np.clip(frac_positions, 0, 1)
-            
-            # interpolate onto fixed lifecycle grid
-            f = interp1d(frac_positions, pctg_contr, bounds_error=False, fill_value=np.nan)
-            bin_values[tr] = f(lifecycle_positions)
 
+            total_cr = valid_cr.sum()
+            total_pr = valid_pr.sum()
+            pctg_cr_contr = (valid_cr / total_cr) * 100
+            pctg_pr_contr = (valid_pr / total_pr) * 100
+
+            dur_hours = durations_hours[tr]
+            valid_indices = np.where(valid_mask)[0]
+            frac_positions = (valid_indices * 3) / dur_hours
+            frac_positions = np.clip(frac_positions, 0, 1)
+
+            f = interp1d(frac_positions, pctg_cr_contr, bounds_error=False, fill_value=(np.nan, pctg_cr_contr[-1]))
+            f_pr = interp1d(frac_positions, pctg_pr_contr, bounds_error=False, fill_value=(np.nan, pctg_pr_contr[-1]))
+            cr_bin_values[tr] = f(lifecycle_positions)
+            pr_bin_values[tr] = f_pr(lifecycle_positions)
+        
         lifecycle_mean_cr_contr = np.array([
-                np.nanmean(bin_values[:, b]) if not np.all(np.isnan(bin_values[:, b])) else np.nan 
+                np.nanmean(cr_bin_values[:, b]) if not np.all(np.isnan(cr_bin_values[:, b])) else np.nan 
                 for b in range(n_bins)
             ])
+        lifecycle_mean_pr_contr = np.array([
+                        np.nanmean(pr_bin_values[:, b]) if not np.all(np.isnan(pr_bin_values[:, b])) else np.nan 
+                        for b in range(n_bins)
+                    ])
+        lifecycle_mean_cr_contr = lifecycle_mean_cr_contr / lifecycle_mean_cr_contr.sum() * 100  ## accounts for the interpolation error
+        lifecycle_mean_pr_contr = lifecycle_mean_pr_contr / lifecycle_mean_pr_contr.sum() * 100  ## accounts for the interpolation error
         lifecycle_pctg = np.linspace(0, 100, n_bins)
 
         ax.plot(lifecycle_pctg, lifecycle_mean_cr_contr, color=color, label=mname)
+        ax1.plot(lifecycle_pctg, lifecycle_mean_pr_contr, color=color, linestyle='--')
 
-    ax.legend(bbox_to_anchor = (1.3, 1.2), ncols=4)
-    ax.set_xlabel(r"$\%$ of MCS lifecyle")
+    # ax.legend(bbox_to_anchor = (1.3, 1.2), ncols=4)
+    ax.plot([], [], color='grey', alpha=0.5, linestyle= '--', label='precipitation')
+    ax.plot([], [], color='grey', alpha=0.5, linestyle= '-', label='condensation')
+    ax.set_xlabel(r"$\%$ MCS lifecyle")
     ax.set_ylabel(r"$\%$ Contribution to total condensation")
+    ax1.set_ylabel(r"$\%$ Contribution to total precipitation")
     ax.set_xticks(np.arange(0, 110, 10))
+    for _ax in [ax, ax1]: 
+        _ax.set_yticks(np.arange(2, 7, 1))
+        _ax.set_ylim(1.5, 6)
+    ax.spines['right'].set_visible(True)
     # ax.set_ylim(0.2, 1.)
     ax.grid(color='white')
-    plt.savefig('figs/lifecycle_cr_contribution.png', bbox_inches='tight')
+    p_utils.centre_legend_above(fig, ax, y_offset=1.06, ncols=3)
+    plt.savefig('figs/lifecycles/crprSUM_contribution_lifecycle')
 
 
 
@@ -167,8 +192,8 @@ def _plot_cr_lifecycle_contribution(n_bins=20):
 ## RUN FILES
 ##########
 
-_plot_lifecycle_mean_cr(n_bins=10)
-_plot_cr_lifecycle_contribution(n_bins=10)
+_plot_lifecycle_mean_cr(n_bins=24)
+_plot_cr_pr_lifecycle_contribution(n_bins=24)
 
 ###########
 
